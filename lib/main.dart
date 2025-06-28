aa06d2afee174ab1b8c98e80feeb12b8709fd2fd
Nikesh <bhandarinikesh93@gmail.com>
2025-06-28 21:24:21 +0545

    routing for otp and dash

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:fohormalai/features/auth/login_view.dart';
+import 'package:fohormalai/features/auth/opt_view.dart';
 import 'package:fohormalai/features/auth/register_view.dart';
+import 'package:fohormalai/features/home/dashboard.dart';
 
 void main() {
   runApp(const FohorMalaiApp());
@@ -21,8 +23,11 @@ class FohorMalaiApp extends StatelessWidget {
       ),
       initialRoute: '/register',
       routes: {
+        // '/': (context) => HomePage(),
         '/register': (context) => const RegisterPage(),
         '/login': (context) => const LoginPage(),
+        '/otp': (context) => const OtpView(),
+        "/dashboard": (context) => const Dashboard(),
       },
     );
   }
